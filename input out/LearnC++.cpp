203cb232dcf785d8058dc9dcbbb72af4baa4b93d
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-08-14 20:20:00 +0530

    Add files via upload

---
+++ input out/LearnC++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;    
+
+int main(){
+    // cout<<"Hey Sweata"<<endl;
+    // cout<<"hey Sweata"<<endl;
+    int x,y;
+    cin>>x>>y;
+    cout<<"Value of x: "<<x<<endl<<"and y: "<<y<<endl;
+    return 0;
+
+}
