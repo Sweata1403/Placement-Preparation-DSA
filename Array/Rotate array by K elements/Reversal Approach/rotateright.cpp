a37ed54976998ebd9ecb652b9f6f1d126246ef3e
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-10-03 20:15:04 +0530

    Add files via upload

---
+++ Array/Rotate array by K elements/Reversal Approach/rotateright.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//Function to reverse the array
+void Reverse(int arr[], int start, int end){
+    while(start<=end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+//Function to Rotate K elements to right
+void Rotateeletoright(int arr[], int n, int k){
+    //Reverse first n-k elements
+    Reverse(arr, 0, n-k-1);
+    //Reverse last k elements
+    Reverse(arr, n-k, n-1);
+    //Reverse whole array
+    Reverse(arr, 0, n-1);
+}
+int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    int n=7;
+    int k=2;
+    Rotateeletoright(arr,n,k);
+    cout<<"After rotating the k elements to right: "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
